21536c34e8b3922d8723e311d4a0d2f7ce531c00
ClementRollin <clemrollin01@gmail.com>
2025-11-07 16:44:40 +0100

    fix errors ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,18 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { FlatCompat } from '@eslint/eslintrc'
+import pluginVue from 'eslint-plugin-vue'
+import prettierConfig from '@vue/eslint-config-prettier'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const compat = new FlatCompat({ baseDirectory: __dirname })
-
-export default [
-  ...compat.config({
-    env: { node: true, browser: true },
-    extends: [
-      'plugin:vue/vue3-recommended',
-      '@vue/eslint-config-typescript',
-      '@vue/eslint-config-prettier',
-    ],
-    parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
-    rules: { 'vue/multi-word-component-names': 'off' },
-  }),
-]
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommendedTypeChecked,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+    ignores: ['dist', 'coverage', 'test-results'],
+  },
+  prettierConfig,
+)
