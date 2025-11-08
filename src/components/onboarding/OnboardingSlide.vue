a9a10cfef648e55e6bbcbd68c3809b12e8fcd78b
ClementRollin <clemrollin01@gmail.com>
2025-11-08 10:51:25 +0100

    Update config tailwind

--- src/components/onboarding/OnboardingSlide.vue
+++ src/components/onboarding/OnboardingSlide.vue
@@ -3,8 +3,8 @@
     <img v-if="image" :src="image" :alt="imageAlt || title"
          class="rounded-200 mb-4 w-full h-56 md:h-72 object-cover" />
     <div class="flex-1 flex flex-col w-full">
-      <h3 class="text-2xl font-extrabold mb-2">{{ title }}</h3>
-      <p class="opacity-90 leading-relaxed">{{ description }}</p>
+      <h3 class="font-sans text-2xl mb-2">{{ title }}</h3>
+      <p class="opacity-90 leading-relaxed font-sans typography-text">{{ description }}</p>
     </div>
   </article>
 </template>
