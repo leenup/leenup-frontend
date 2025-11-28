d52bb5e949709ac7b37813f66deba64f8e242bcf
ClementRollin <clemrollin01@gmail.com>
2025-11-28 15:05:25 +0100

    deploy project to vercel

---
+++ build/js/AboutView-B9_tIVPv.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./index-D7hDrqer.js";const s={},t={class:"p-4"};function n(r,_){return c(),o("section",t,"À propos de Leenup")}const p=e(s,[["render",n]]);export{p as default};
